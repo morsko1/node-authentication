473f83854716fe505f971916a1796cc879d88c49
morsko1 <morssko1@gmail.com>
2023-05-02 18:12:25 +0600

    init prisma

---
+++ src/db/dbClient.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+// add prisma to the global type
+interface CustomNodeJsGlobal extends Global {
+  prisma: PrismaClient;
+}
+
+// prevent multiple instances of Prisma Client in development
+declare const global: CustomNodeJsGlobal;
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') {
+  global.prisma = prisma;
+}
+
+export default prisma;
--- src/routes/ping.ts
+++ src/routes/ping.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import db from '../db/dbClient';
 
 const ping = Router();
 
 ping.get('/', (_req, res) => {
   res.send('pong');
+});
+
+ping.get('/users', async (_req, res) => {
+  const allUsers = await db.user.findMany();
+  res.json(allUsers);
 });
 
 export { ping };
